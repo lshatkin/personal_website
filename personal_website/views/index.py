a3b63e6f1f15cdc07946eaaa86f6019faa671f31
lshatkin <lshatkin@umich.edu>
2020-03-13 08:31:49 -0500

    add changes

--- personal_website/views/index.py
+++
@@ -1,8 +0,0 @@
-import flask
-import personal_website
-
-@app.route('/')
-def index():
-    age = int((datetime.date.today() - datetime.date(1995, 4, 22)).days / 365)
-    return flask.render_template('home.html', age=age)
-
